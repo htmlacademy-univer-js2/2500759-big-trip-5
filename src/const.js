12e5bb07b49f8f2b6b8bb46dacfd92361766ec0b
Алексей Борщ <borschalex24@gmail.com>
2025-04-01 22:35:55 +0500

    4.17. Шаблонизируй то

--- src/const.js
+++ src/const.js
@@ -1,3 +1,4 @@
+import { pointFuture, pointPast, pointPresent } from './utils';
 export const POINT_TYPE = ['taxi', 'bus', 'ship', 'train', 'flight', 'drive', 'check-in', 'sightseeing', 'restaurant'];
 export const DATE_FORMAT = {
   'full-date': 'YYYY-MM-DD',
@@ -5,4 +6,18 @@ export const DATE_FORMAT = {
   'hours-minutes': 'HH:mm',
   'full-date-and-time': 'YYYY-MM-DDTHH:MM',
   'full-date-and-time-slash': 'DD/MM/YYYY HH:mm',
+};
+
+export const FILTER_TYPES = {
+  EVERYTHING: 'everything',
+  FUTURE: 'future',
+  PRESENT: 'present',
+  PAST: 'past',
+};
+
+export const FILTER_GENERATOR = {
+  [FILTER_TYPES.EVERYTHING]: (points) => [...points],
+  [FILTER_TYPES.PAST]: (points) => points.filter((point) => pointPast(point)),
+  [FILTER_TYPES.PRESENT]: (points) => points.filter((point) => pointPresent(point)),
+  [FILTER_TYPES.FUTURE]: (points) => points.filter((point) => pointFuture(point)),
 };
